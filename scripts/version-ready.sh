81486398870b845c13048974b5ab9442e0bc04dd
Omri Bernstein <hippiccolo@gmail.com>
2016-03-30 16:29:47 -0400

    Update version-ready script to include dist into any version update commit

--- scripts/version-ready.sh
+++ scripts/version-ready.sh
@@ -1,5 +1,7 @@
-(npm run build && npm test && bash "$(dirname ${BASH_SOURCE[0]})/push-ready.sh")
+(npm run build && git add dist && npm test && bash "$(dirname ${BASH_SOURCE[0]})/push-ready.sh")
 if [[ $? -ne 0 ]]; then
+  git reset HEAD dist
+  git checkout -- dist
   printf "\e[31m×\e[0m Address above issues before bumping version"
   exit 1
 fi
